a3d56eb65e3748173fbec09abfda0a1150e8a7a6
Alex Bonwit <alexbonwit@mac.com>
2019-11-15 23:51:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_hash = {}
+  
+  data.each do |attribute, values|
+  
+    values.each do |key, names|
+    
+      names.each do |string| 
+        if !pigeon_hash[string] 
+        pigeon_hash[string] = {}
+        end
+        if !pigeon_hash[string][attribute]
+          pigeon_hash[string][attribute] = []
+        end
+        pigeon_hash[string][attribute] << key.to_s
+      end
+      
+    end
+  end
+  # binding.pry
+  pigeon_hash
 end
