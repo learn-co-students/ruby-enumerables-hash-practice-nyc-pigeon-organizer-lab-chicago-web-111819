6ad34d95fa32c880343709f093bc0ad32f6c73ef
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 20:24:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,46 +1,51 @@
 #require 'pry'
 
-def nyc_pigeon_organizer (data)
-  final = {}
-
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
-      array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
+def nyc_pigeon_organizer(data)
+  good_data = {}
+  
+  data.each do |key, value|
+    value.each do |key_2, names|
+      names.each do |name| 
+        good_data[name] = {:color => [], :gender => [], :lives => []}
       end 
-    end 
+    end   
   end 
-  x = final.keys
+  
+  keys_good_data = good_data.keys
+ 
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
-      x.each do |item|
+      keys_good_data.each do |item|
         if bird_name === item
-          final[item][:color] << bird_color.to_s
+          good_data[item][:color] << bird_color.to_s
         end 
       end 
     end 
-  end 
+  end  
+  
   data[:gender].each do |gender, type|
     type.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:gender] << gender.to_s
-        end 
-      end 
-    end 
-  end 
-  data[:lives].each do |location, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:lives] << location
-        end 
+      keys_good_data.each do |value_in|
+        if bird_name === value_in
+          good_data[value_in][:gender] << gender.to_s
+        end
       end 
     end 
   end 
   
-  return final 
-end 
+  data[:lives].each do |home, name|
+    name.each do |bird_name|
+      keys_good_data.each do |value_in|
+        if bird_name === value_in
+          good_data[value_in][:lives] << home
+        end
+      end 
+    end 
+  end 
+  
+  
+  return good_data
+end
 
 
 
